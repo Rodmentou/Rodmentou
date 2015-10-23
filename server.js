26752dec3e686aa03928e2a7fcd23d6f320a5412
Rodrigo <rodmentou@gmail.com>
2015-10-22 21:58:11 -0300

    'a'

--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var express = require('express'),
 var env = 'dev';
 var port = 80;
 
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With, content-type, Authorization');
+  next();
+});
+
 app.get('/', function (req, res) {
 	res.send('Hello there!');
 });
