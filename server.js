c9f1b732148a7e74c59474203a2abd9d942995d5
Rodrigo <rodmentou@gmail.com>
2015-10-27 21:56:15 -0300

    Configuring back end.

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express'),
 	app = express(),
-	bodyParser = require('body-parser');
+	bodyParser = require('body-parser'),
+	mongoose = require('mongoose'),
+	jwt = require('express-jwt');
 
-app.env = 'dev';
-app.port = process.env.PORT || 5000;
+app.ENV = 'dev';
+app.PORT = process.env.PORT || 5000;
+//app.JWT_SECRET = process.env.JWT_SECRET || {};
+app.DB_URL = process.env.DB_URL || {};
+app.CLIENT_SECRET = process.env.CLIENT_SECRET || {};
+app.CLIENT_ID = process.env.CLIENT_ID || {};
+
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -20,13 +27,19 @@ app.use(express.static(__dirname + '/public'));
 
 var api = express.Router();
 
+var jwtCheck = jwt({
+	secret: new Buffer(CLIENT_SECRET, 'base64'),
+	audience: CLIENT_ID
+});
+
 app.get('/', function (req, res) {
 	console.log("Someone accessed me!");
 	res.send('Olá, delícia!');
 });
 
+app.use('/api', jwtCheck);
 app.use('/api', api);
 
-app.listen(app.port, function () {
-	console.log('Server running on ' + app.env + ' at ' + app.port + '.');
+app.listen(app.PORT, function () {
+	console.log('Server running on ' + app.ENV + ' at ' + app.PORT + '.');
 });
