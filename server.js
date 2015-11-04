f42121487d83403d5d32a6164888237ca1b5d9e6
Rodrigo <rodmentou@gmail.com>
2015-11-04 19:17:38 -0300

    Some progress with auth0

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 var express = require('express'),
 	app = express(),
 	bodyParser = require('body-parser'),
-	mongoose = require('mongoose');
+	mongoose = require('mongoose'),
+	jwt = require('express-jwt');
 
 app.ENV = process.env.IS_PROD || false;
 app.PORT = process.env.PORT || 5000;
 app.DB_URL = process.env.DB_URL;
+app.SECRET = process.env.SECRET || '';
+app.AUDIENCE = process.env.AUDIENCE || '';
 
-
+var jwtCheck = jwt({
+  secret: new Buffer(app.SECRET, 'base64'),
+  audience: app.AUDIENCE
+});
 
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
@@ -31,8 +37,8 @@ app.get('/', function (req, res) {
 	res.json({success: true, message: 'Olá, delícia!'});
 });
 
+app.use('/api', jwtCheck);
 require('./routes/user')(api);
-app.use('/api', api);
 
 app.get('/api', function (req, res) {
 	res.json({success: true, message: 'Wellcome to the API'});
